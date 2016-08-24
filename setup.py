c2e8b1b84b19f0d3602223fea68e1537ac0a70ca
James Cheese <jamesc@intouch-ltd.com>
2016-08-24 12:24:05 +0100

    0.1.1: Bumping version for doc changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="dump_xml",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/intouch-smartwater/dump_xml",
 
     author="James Cheese",
